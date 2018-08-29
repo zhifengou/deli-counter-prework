3d9a7cf6a820e511cf6c3bb1462c6bec90a46a4b
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-29 00:47:12 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,5 @@
 # Write your code here.
 katz_deli=[]
-# def line(array) 
-#   if array.length == 0 
-#     puts "The line is currently empty."
-#   else 
-#     message = "The line is currently:"
-    
-#   array.each_with_index do |value, index| 
-#     message += " #{index.to_i+1}. #{value}"
-#   end 
-#   puts "#{message}"
-#   end 
-# end 
 def line(arr)
   if arr.size==0 
     puts "The line is currently empty."
